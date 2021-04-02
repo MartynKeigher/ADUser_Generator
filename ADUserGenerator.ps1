862ce80c4f2904be9e3ad80e03bc3b87793d5852
Martyn T. Keigher <martyn@martynkeigher.com>
2021-04-02 00:01:01 -0400

    Update ADUserGenerator.ps1

--- ADUserGenerator.ps1
+++ ADUserGenerator.ps1
@@ -5,13 +5,13 @@ function ADUser-Generation {
         [string] $Company,
         [parameter(Mandatory=$false,HelpMessage="Please enter the name of the OU you want to place your users. (Default: 'Staff')")] 
         [string] $OU = 'Staff',
-        [parameter(Mandatory=$false,HelpMessage="Please enter a 3 digit area code. (Default: '727'")]
+        [parameter(Mandatory=$false,HelpMessage="Please enter a 3 digit area code. (Default: '727')")]
         [ValidateRange(201,989)] 
         [string] $AreaCode = 727,
         [parameter(Mandatory=$false,HelpMessage="Number of user accounts to generate. (Default:15;Max: 1500)")] 
         [ValidateRange(1,1500)] 
         [int] $UserCount = 15,
-        [parameter(Mandatory=$false,HelpMessage="Please enter a password for the generated users. (Default: 'P@ssw0rd1'")]
+        [parameter(Mandatory=$false,HelpMessage="Please enter a password for the generated users. (Default: 'P@ssw0rd1')")]
         [String] $Password = 'P@ssw0rd1'
     )
 
